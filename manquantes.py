7d32cc1d319434b30079234b2446aece5c31e2c4
godwin <godoblasse@gmail.com>
2025-03-18 15:05:17 +0100

    missing values and graph of missing values i know it is zero :)

---
+++ manquantes.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("kc_house_data.csv")
+
+missing_values = df.isnull().sum()
+print("Nombre de valeurs manquantes par colonne :\n", missing_values)
+
+missing_percentage = (df.isnull().sum() / len(df)) * 100
+print("Pourcentage de valeurs manquantes par colonne :\n", missing_percentage)
+
+df = pd.read_csv("kc_house_data.csv")
+
+plt.figure(figsize=(10, 6))
+sns.heatmap(df.isnull(), cmap="viridis", cbar=False, yticklabels=False)
+plt.title("Carte des valeurs manquantes")
+plt.show()
