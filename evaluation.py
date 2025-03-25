73509e98e0cce8d905a145b031f26279d1294b58
godwin <godoblasse@gmail.com>
2025-03-25 16:56:51 +0100

    other regressions

--- evaluation.py
+++ evaluation.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error, mean_absolute_error
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 
 df = pd.read_csv("kc_house_data.csv")
 
@@ -23,10 +23,13 @@ reg.fit(X_train, y_train)
 y_pred = reg.predict(X_test)
 
 print("Prédictions des prix : ", y_pred[:5])
-print("vrai prix : ", y_test[:5])
+print("Vrai prix : ", y_test[:5])
 
 mse = mean_squared_error(y_test, y_pred)
 rmse = mse**0.5 
 mae = mean_absolute_error(y_test, y_pred)
+r2 = r2_score(y_test, y_pred)
 
 print("RMSE:", rmse)
+print("MAE:", mae)
+print("R²:", r2)
